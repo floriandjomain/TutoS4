75f552bd325fbc0ea84054da77ba59093e01ca27
flupi <floriand.jomain@gmail.com>
2019-03-26 17:13:09 +0100

    maj

--- gitadd.sh
+++ gitadd.sh
@@ -1,6 +1,6 @@
 #On s'assure d'être à jour concernant les différentes versions du projet qui est en ligne
 ./gitmaj.sh
 #On remonte ensuite les modifications à apporter au projet
-git add *
+git add .
 git commit -m $1
 git push
